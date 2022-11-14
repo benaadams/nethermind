5643d949c7ba6a884d9cc69d2c67d919f080dfd1
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2022-11-14 15:16:53 +0100

    Added SyncStage monitoring

---
+++ src/Nethermind/Nethermind.Synchronization/Reporting/SnapshotReport.cs
@@ -0,0 +1,39 @@
+//  Copyright (c) 2021 Demerzel Solutions Limited
+//  This file is part of the Nethermind library.
+// 
+//  The Nethermind library is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  The Nethermind library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Linq;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Microsoft.VisualBasic;
+using Nethermind.Synchronization.ParallelSync;
+
+namespace Nethermind.Synchronization.Reporting
+{
+    public class SyncReportSymmary
+    {
+        public string CurrentStage { get; set; }
+    }
+    public static class ReportSink
+    {
+        public static SyncMode CurrentStage { get; set; } = SyncMode.None;
+        public static SyncReportSymmary Snapshot => new SyncReportSymmary
+        {
+            CurrentStage = CurrentStage.ToString(),
+        };
+    }
+}
